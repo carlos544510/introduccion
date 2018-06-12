e708b099617ada4d64a670b50e8f29d72c9ddee2
carlos544510 <karlos.diaz54@gmail.com>
2018-06-12 01:03:32 -0500

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserCrudComponent } from './user-crud/user-crud.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserAddComponent } from './user-add/user-add.component';
 
 const routes: Routes = [
   {path: 'crud',component: UserCrudComponent},
   {path: 'users/:id/detail',component: UserDetailComponent},
-  {path: 'users/:id/edit',component: UserEditComponent}
+  {path: 'users/:id/edit',component: UserEditComponent},
+  {path: 'users/add',component: UserAddComponent}
 ];
 
 @NgModule({
--- src/app/user-add/user-add.component.ts
+++ src/app/user-add/user-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user-add',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-add.component.css']
 })
 export class UserAddComponent implements OnInit {
-
+  public user: User;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(name: string, telephone: string){
+    console.log(name);
+    const url = 'http://localhost:4000/api/v1/users';
+    var formData = new FormData();
+    formData.append("name", name);
+    formData.append("telephone", telephone);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.send(formData);
+
   }
 
 }
